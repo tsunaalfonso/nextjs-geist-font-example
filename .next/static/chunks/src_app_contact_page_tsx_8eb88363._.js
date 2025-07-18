5928dbf4addeb8194a41847e71fcdf2819cc8ca1
KellinQuinn01 <47975483+tsunaalfonso@users.noreply.github.com>
2025-07-19 03:46:22 +0800

    Add .next/static/chunks/src_app_contact_page_tsx_8eb88363._.js 1752867981910

---
+++ .next/static/chunks/src_app_contact_page_tsx_8eb88363._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_28c4a3c0._.js",
+  "static/chunks/src_7923c247._.js"
+],
+    source: "dynamic"
+});
